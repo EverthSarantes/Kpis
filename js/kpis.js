127ceeeee6303ad9af47a890c6b036578a16c32d
everth <everthleonardosaran@gmail.com>
2024-04-10 18:37:18 -0600

    first commit

---
+++ js/kpis.js
@@ -0,0 +1,11 @@
+//funciones Everth
+
+function calcularTasaFallas(numero_fallas, tiempo_operacion){
+    // Tasa de fallas = (Número total de fallas / Tiempo total de operación en dias) * 100
+    return (numero_fallas / tiempo_operacion) * 100;
+}
+
+function calcularTiempoMedioFallas(){
+    
+}
+
